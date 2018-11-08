cd0a37c0f6ccf48fd85199b2e85f4af8c34efe2f
zouyang0921 <zouyang0921@gmail.com>
2018-11-08 23:46:25 +0800

    start project

---
+++ build/webpack.config.base.js
@@ -0,0 +1,38 @@
+const path = require('path')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+  target: 'web',
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.[hash:8].js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: '[name]-aaa.[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = config
