4e8067947de1d879806b1e791d08fd0c29018d8d
zouyang0921 <zouyang0921@gmail.com>
2018-11-09 10:34:28 +0800

    update webpack config

---
+++ build/webpack.config.client.js
@@ -0,0 +1,111 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+const baseConfig = require('./webpack.config.base')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const defaultPlugins = [
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: isDev ? '"development"' : '"production"'
+        }
+    }),
+    new HTMLPlugin({
+        template: path.join(_dirname, 'template.html')
+    })
+]
+
+const devServer = {
+    port: 8000,
+    host: '0.0.0.0',
+    overlay: {
+        errors: true
+    },
+    headers: { 'Access-Control-Allow-Origin': '*'},
+    historyApiFallback: {
+        index: '/public/index.html'
+    },
+    proxy: {
+        '/api': 'http://127.0.0.1:3333',
+        '/user': 'http://127.0.0.1:3333'
+    },
+    hot: true
+}
+
+let config
+
+if (isDev) {
+    config = merge(baseConfig, {
+        devtool: '#cheap-module-eval-source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.less/,
+                    use: ExtractPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            'css-loader',
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            },
+                            'less-loader'
+                        ]
+                    })
+                }
+            ]
+        },
+        devServer,
+        plugins: defaultPlugins.concat([
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoEmitOnErrorsPlugin()
+        ])
+    })
+} else {
+    config = merge(baseConfig, {
+        entry: {
+            app: path.join(__dirname, 'src/index.js'),
+            vendor: ['vue']
+        },
+        output: {
+            filename: '[name].[chunkhash:8].js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.less/,
+                    use: ExtractPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            'css-loader',
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            },
+                            'less-loader'
+                        ]
+                    })
+                }
+            ]
+        },
+        plugins: defaultPlugins.concat([
+            new ExtractPlugin('styles.[contentHash:8].css'),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'vendor'
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'runtime'
+            }),
+            new webpack.NamedChunksPlugin()
+        ])
+    })
+}
+
+module.exports = config
